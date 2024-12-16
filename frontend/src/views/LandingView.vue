875984ba9a2b727a664f4212f85e815fc8bca10f
Brian Both <bboth@uci.edu>
2024-12-16 15:59:43 -0800

    added tailwind configuration

--- frontend/src/views/LandingView.vue
+++ frontend/src/views/LandingView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="login">
-    <h1>This is a Login Page</h1>
+    <h1>This is a Landing Page</h1>
   </div>
 </template>
 
--- frontend/src/views/SignupView.vue
+++ frontend/src/views/SignupView.vue
@@ -1,7 +1,9 @@
+<script>
+function handleSignup(e) {}
+</script>
+
 <template>
   <div class="signup">
     <h1>This is a Signup Page</h1>
   </div>
 </template>
-
-<style></style>
