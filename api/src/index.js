0a2c9842a2fa4117fc0f8ad25198f8fb9ccf041d
nishant <nishun2005@gmail.com>
2024-12-15 20:42:16 -0800

    init api

--- api/src/index.js
+++ api/src/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const http = require('http')
-
+const cors = require('cors');
+const compression = require('compression');
 
 const IP = '0.0.0.0';
 const PORT = 5555;
 const app = express();
 app.use(compression());
-app.use(cors({ origin: ORIGIN }));
+app.use(cors({ origin: '*' }));
 const server = http.createServer(app);
 
 server.listen(PORT, IP, () => {
