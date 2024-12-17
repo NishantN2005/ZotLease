9918b8c3d1e797597b4781228eb0332fc6632dd5
Brian Both <bboth@uci.edu>
2024-12-17 11:25:41 -0800

    added leaflet map component

---
+++ frontend/src/components/icons/LeafletMap.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="map" style="height: 400px; width: 100%"></div>
+</template>
+
+<script>
+import { onMounted } from 'vue'
+import 'leaflet/dist/leaflet.css'
+import leaf from 'leaflet'
+
+export default {
+  name: 'LeafletMap',
+  setup() {
+    onMounted(() => {
+      const map = leaf.map('map').setView([33.644, -117.826], 15)
+
+      // Add a tile layer (e.g., OpenStreetMap)
+      leaf
+        .tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          maxZoom: 19,
+          attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        })
+        .addTo(map)
+    })
+  },
+}
+</script>
+
+<style>
+#map {
+  height: 400px;
+  width: 100%;
+}
+</style>
--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="dashboard">
     <h1>This is a Dashboard Page</h1>
+    <LeafletMap />
   </div>
 </template>
+
+<script>
+import LeafletMap from '@/components/icons/LeafletMap.vue'
+
+export default {
+  name: 'DashboardView',
+  components: {
+    LeafletMap,
+  },
+}
+</script>
 
 <style></style>
