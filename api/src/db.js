d99f2038bcc8869bde1b8befb1f8cda43b5e3084
nishant <nishun2005@gmail.com>
2025-01-02 23:39:15 -0800

    fix escape

--- api/src/db.js
+++ api/src/db.js
@@ -5,16 +5,16 @@ require("dotenv").config();
 const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  database: "",
+  database: "postgres",
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   max: 10,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
-  ssl: {
-    rejectUnauthorized: false,
-    ca: fs.readFileSync("src/rds-combined-ca-bundle.pem").toString(), // if you're using SSL
-  },
+  // ssl: {
+  //   rejectUnauthorized: false,
+  //   ca: fs.readFileSync("src/rds-combined-ca-bundle.pem").toString(), // if you're using SSL
+  // },
 });
 // (async () => {
 //   //await client.connect()
