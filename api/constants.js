52fa28bf743c77b3e081107c68f78661ed6c92d8
nishant <nishun2005@gmail.com>
2025-01-04 03:03:20 -0800

    include ssl certificate and dynamically change path

--- api/constants.js
+++ api/constants.js
@@ -8,5 +8,6 @@ if (ORIGIN.endsWith('/')) {
 const PORT = 5555;
 const IP = '0.0.0.0';
 
+const SSL_PATH = process.env.SSL_PATH;
 
-module.exports={ORIGIN,IP,PORT, ENVIRONMENT};
+module.exports={ORIGIN,IP,PORT, ENVIRONMENT, SSL_PATH};
