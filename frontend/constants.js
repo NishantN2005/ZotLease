d3474dad883c3a1559a71a41c4a51c3c2dbcd3a6
nishant <nishun2005@gmail.com>
2025-01-04 00:58:08 -0800

    add api url dynamically

--- frontend/constants.js
+++ frontend/constants.js
@@ -1,6 +1,6 @@
 //define environment code is running in
 const ENVIRONMENT = import.meta.env.VITE_ENV
 
-export const API_URL = ENVIRONMENT === 'dev' ? 'http://localhost:5555' : 'http://zotlease-api.service.local' 
+export const API_URL = import.meta.env.VITE_API_URL
 
 export const PHOTO_BUCKET = `zotlease-${ENVIRONMENT}-photoupload`;
