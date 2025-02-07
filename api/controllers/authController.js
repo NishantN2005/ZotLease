5692378d4bba918bb57435a25dedc1eb34a1859a
nishant <nishun2005@gmail.com>
2025-02-07 01:26:46 -0800

    same site to none

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -66,7 +66,7 @@ const loginController = async (req, res) => {
   res.cookie("token", refreshToken, {
     httpOnly: true,
     secure: true,
-    sameSite: "Lax",
+    sameSite: "None",
     maxAge: 24 * 60 * 60 * 1000,
   });
 
@@ -191,7 +191,7 @@ const signupController = async (req, res) => {
 const logoutController = async (req, res) => {
   console.log('inside logout')
   console.log(req.cookies.token);
-  console.log(req.headers.authorization.split(' ')[1], 'cookie here');
+
   if (req.cookies?.token) {
     const refreshToken = req.cookies.token;
     console.log("refresh here", refreshToken);
