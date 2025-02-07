870edff0cc644bfe3c36c95821ae7d06b1f7b492
nishant <nishun2005@gmail.com>
2025-02-07 01:34:39 -0800

    Set from None to lax

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -172,7 +172,7 @@ const signupController = async (req, res) => {
     res.cookie("token", refreshToken, {
       httpOnly: true,
       secure: true,
-      sameSite: "Lax",
+      sameSite: "None",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
