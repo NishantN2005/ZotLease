114badefcabd28c8acfb24680228a9270556e1f1
nishant <nishun2005@gmail.com>
2025-02-26 17:15:16 -0800

    google analytics

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,8 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+import VueCookieConsent from 'vue-cookieconsent'
 
 import App from './App.vue'
 import router from './router'
@@ -12,5 +13,12 @@ const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 app.use(pinia)
 app.use(router)
+
+app.use(VueCookieConsent, {
+    // Customize options if necessary
+    cookieName: 'yourCookieConsent',
+    autoAccept: false, // Optionally require user interaction
+    // You can also customize the banner text, button text, colors, etc.
+  })
 
 app.mount('#app')
