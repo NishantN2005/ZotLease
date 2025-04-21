48d2b5bb0f8c31e2bbc22f50e4afa1fde5bbbe08
arnavgosain <arnav.gosain@gmail.com>
2025-04-21 15:16:24 -0700

    added incorrect box highlighting

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -45,7 +45,7 @@ export const makeAuthenticatedRequest = async (endpoint, data, router, methodTyp
     body: JSON.stringify(data),
   })
 
-  if (response.ok) {
+  if (response.ok || response.status === 400) {
     return response
   } else {
     if (response.status === 401) {
